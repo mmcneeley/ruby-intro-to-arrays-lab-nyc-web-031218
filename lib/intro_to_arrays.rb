9f37a911db71585f479af3c21a221b695304176f
Mike McNeeley <mmcneeley@gmail.com>
2018-02-28 18:11:44 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -23,11 +23,11 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  first_element
+  first_element(array)
 end
 
 def last_element_with_array_methods(array)
-  last_element
+  last_element(array)
 end
 
 def length_of_array(array)
