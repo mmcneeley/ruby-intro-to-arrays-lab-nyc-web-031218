daf4bc0c9303f7b03064e6df23b0aa2dc5c21961
Mike McNeeley <mmcneeley@gmail.com>
2018-02-28 18:07:19 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  []
+end
+
+my_new_array = instantiate_new_array
+
+def array_with_two_elements
+  [1, 2]
+end
+
+my_two_array = array_with_two_elements
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
